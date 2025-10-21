940307048b37badfcd90bf1771de286e7cbd119d
Imad <karimimadbz@gmail.com>
2025-10-21 16:03:04 +0200

    helpPageProgress

--- src-react/App.jsx
+++ src-react/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import HelpPage from "./pages/help/HelpPage";
 
 
 export default function App() {
@@ -24,6 +25,7 @@ export default function App() {
 
     return (
         <div>
+            <HelpPage/>
         </div>
     );
 }
