eb0b682ac17f3667accfcefeaa2a26f553486a8b
Imad <karimimadbz@gmail.com>
2025-10-23 08:49:17 +0200

    HelpPages

--- src-react/pages/help/HelpPage.jsx
+++ src-react/pages/help/HelpPage.jsx
@@ -1,3 +1,4 @@
+import { ThemeItem } from './ThemeItem';
 import React from "react";
 import MainLayout from "../../components/layout/MainLayout";
 import SearchBar from "../../components/ui/SearchBar";
@@ -8,8 +9,8 @@ import { FileText, GraduationCap, Building2, File } from 'lucide-react';
 function HelpPage() {
     
     return ( 
-            <MainLayout>
-                <div className="w-full">
+            <MainLayout >
+                <div className="flex w-full justify-center">
                     <SearchBar className={"w-[90%] md:w-[80%]"} />
                 </div>
 
@@ -19,21 +20,21 @@ function HelpPage() {
                        Fréquemment demandées
                     </h5>
 
-                    <div className=" mt-5 flex flex-row gap-5 justify-center">
+                    <div className=" mt-5 flex flex-row gap-5 justify-center overflow-x-auto p-4 snap-x snap-mandatory">
 
-                        <Container className="bg-[#BEF264]">
+                        <Container className="bg-[#BEF264] rounded shadow snap-start">
                         <p>Où puis-je télécharger la fiche <br /> de renseignement ? </p>
                         </Container>
 
-                        <Container className="bg-[#BEF264]">
+                        <Container className="bg-[#BEF264] rounded shadow snap-start">
                         <p>Comment remplir la fiche de <br /> renseignement ?</p>
                         </Container>
 
-                        <Container className="bg-[#BEF264]">
+                        <Container className="bg-[#BEF264] rounded shadow snap-start">
                         <p>Comment suivre l'avancée <br /> des signatures </p>
                         </Container>
 
-                        <Container className="bg-[#BEF264]">
+                        <Container className="bg-[#BEF264] rounded shadow snap-start">
                         <p>Comment sont organisés les <br /> heures en entreprise ?</p>
                         </Container>
 
@@ -47,25 +48,13 @@ function HelpPage() {
                        Thèmes
                     </h5>
 
-                    <div className="flex flex-col gap-5 mt-5">
-                        <Container>
-                            <FileText />
-                            <h5>Fiche de renseignements</h5>
-                            <p>2 articles</p>
-                        </Container>
-                        <Container>
-                            <GraduationCap />
-                            <h5>Fiche de renseignements</h5>
-                        </Container>
-                        <Container >
-                            <Building2 />    
-                            <h5>Fiche de renseignements</h5>
-                        </Container>
+                    <div className="flex flex-col gap-5 mt-5 justify-center">
 
-                        <Container>
-                            <File />
-                            <h5>Fiche de renseignements</h5>
-                        </Container>
+                        <ThemeItem  NArticles={2} icon={<FileText strokeWidth={1} />} titre={"Fiche de renseignement"}    />
+                        <ThemeItem  NArticles={6} icon={<GraduationCap strokeWidth={1} />} titre={"Stagiaires"}    />
+                        <ThemeItem  NArticles={4} icon={<Building2 strokeWidth={1} />} titre={"Entreprises"}    />
+                        <ThemeItem  NArticles={10} icon={<File strokeWidth={1} />} titre={"Fonctionnement"}    />
+
                         
                     </div>
 
