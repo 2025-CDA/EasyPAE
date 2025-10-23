b7852d280c06e86456f42ebcc7124de3e70493e7
Arnauw <arnaud.rabel@gmail.com>
2025-10-23 13:34:15 +0200

    fix max dtos providers

--- src/Dto/OrganizationDTO.php
+++ src/Dto/OrganizationDTO.php
@@ -10,44 +10,45 @@ use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\GetCollection;
 use App\Enum\InfoFormStatus;
+use App\State\OrganizationProvider;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ApiResource(
     operations: [
         new GetCollection(
             uriTemplate: '/organization/sessions',
-            provider: \App\State\OrganizationProvider::class,
-            name: 'organization_sessions',
             normalizationContext: ['groups' => ['read:organization_sessions']],
+            name: 'organization_sessions',
+            provider: OrganizationProvider::class,
         ),
-        new Get(
-            uriTemplate: '/organization/session/{sessionId}',
-            provider: \App\State\OrganizationProvider::class,
-            name: 'organization_session_detail',
-            normalizationContext: ['groups' => ['read:organization_session_detail']],
-        ),
-        new Patch(
-            uriTemplate: '/organization/session/{sessionId}/edit',
-            provider: \App\State\OrganizationProvider::class, // obligatoire pour charger la ressource
-            processor: OrganizationProcessor::class, // gère la mise à jour
-            name: 'organization_session_edit',
-            denormalizationContext: ['groups' => ['update:organization_session']],
-        ),
+//        new Get(
+//            uriTemplate: '/organization/session/{sessionId}',
+//            normalizationContext: ['groups' => ['read:organization_session_detail']],
+//            name: 'organization_session_detail',
+//            provider: OrganizationProvider::class,
+//        ),
+//        new Patch(
+//            uriTemplate: '/organization/session/{sessionId}/edit',
+//            denormalizationContext: ['groups' => ['update:organization_session']], // obligatoire pour charger la ressource
+//            name: 'organization_session_edit', // gère la mise à jour
+////            provider: OrganizationProvider::class,
+//            processor: OrganizationProcessor::class,
+//        ),
 
         new GetCollection(
             uriTemplate: '/organization/session/{sessionId}/interns',
-            provider: \App\State\OrganizationProvider::class,
-            name: 'organization_session_interns',
             normalizationContext: ['groups' => ['read:organization_session_interns']],
+            name: 'organization_session_interns',
+            provider: OrganizationProvider::class,
         ),
 
-        // new Get(
-        //     uriTemplate: '/organization/session/{sessionId}/interns/{internId}',
-        //     provider: \App\State\OrganizationProvider::class,
-        //     name: 'organization_session_intern_detail',
-        //     normalizationContext: ['groups' => ['read:organization_session_intern_detail']],
-        // )
-        // new Delete(),
+//         new Get(
+//             uriTemplate: '/organization/session/{sessionId}/intern/{internId}',
+//             normalizationContext: ['groups' => ['read:organization_session_intern_detail']],
+//             name: 'organization_session_intern_detail',
+//             provider: \App\State\OrganizationProvider::class,
+//         ),
+//         new Delete(),
     ],
     processor: null
 )]
