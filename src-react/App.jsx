184a33fa2a64570e1a3abd774138d4b41636f077
Arnauw <arnaud.rabel@gmail.com>
2025-10-23 05:15:14 +0200

    sorry patience

--- src-react/App.jsx
+++ src-react/App.jsx
@@ -24,7 +24,10 @@ export default function App() {
     // console.log(data[0])
 
     return (
-            <Stepper />
+            <div className="w-full">
+                <Stepper />
+            </div>
+            
     );
 }
 
