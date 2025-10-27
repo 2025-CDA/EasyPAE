59d54882a4d95d876cc3c433b5dd11a07048a8ec
Patience-Kori1 <koribiram@yahoo.fr>
2025-10-27 10:36:06 +0100

    Update AppTable, Table, Select, CardFormation, CalendarSimple

--- src-react/App.jsx
+++ src-react/App.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Stepper from "./components/ui/stepper/Stepper";
 import FicheStagiaire from "./pages/support_service/FicheStagiaire";
+import AppTable from "./components/ui/Table/AppTable";
+import CardFormation from './components/ui/CardFormation'
+import Select from "./components/ui/Select";
 
 export default function App() {
     // const [showAlert, setShowAlert] = useState(true);
@@ -23,5 +26,11 @@ export default function App() {
 
     // console.log(data[0])
 
-    return <div className="w-full"></div>;
+    return <div className="">
+        {/* <AppTable/> */}
+        {/* <CardFormation/> */}
+        <Select
+            classNameSelect ={"hidden py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"}
+        />
+    </div>;
 }
