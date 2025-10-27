9dd04294d146888fc62c4bb7d49b82fe75686a84
Arnauw <arnaud.rabel@gmail.com>
2025-10-27 09:51:39 +0100

    updated Enums and SQL dump

--- src/Enum/WorkLocation.php
+++ src/Enum/WorkLocation.php
@@ -5,9 +5,7 @@ namespace App\Enum;
 enum WorkLocation: string
 {
     case ON_SITE = 'on_site';
-
     case REMOTE = 'remote';
-
     case HYBRID = 'hybrid';
 
     public function toString(): string
