4f39de76904b919717ad4617b0f4559df7c976ad
M-BEDH <https://github.com/M-BEDH>
2025-10-29 11:06:52 +0100

    Solution sans read:false et output: false, pour gerer l'erreur iri :   // expose l'ID de session créé afin que la plate-forme API puisse générer un IRI pour le DTO renvoyé         $data->sessionId = $session->getId();

--- src/Dto/OrganizationDTO.php
+++ src/Dto/OrganizationDTO.php
@@ -57,8 +57,6 @@ use Symfony\Component\Serializer\Annotation\Groups;
             denormalizationContext: ['groups' => ['create:organization_session_add']],
             name: 'organization_session_add',
             processor: OrganizationProcessor::class,
-            read: false,
-            output: false,
         ),
         new Post(
             uriTemplate: '/organization/session/{sessionId}/intern',
