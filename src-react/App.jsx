ce34e6f885d8dd3662c183dc96da492b1239f509
Patience-Kori1 <koribiram@yahoo.fr>
2025-10-31 17:25:50 +0100

    feat(intern-list): Added dynamic form display with smooth transition - Integrated React state (showForm) to control form visibility - Implemented smooth transition using Tailwind (max-height, opacity, translate) - Replaced display:none (hidden) with animated wrapper kept in the DOM

--- src-react/App.jsx
+++ src-react/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import InternListPageOne from './components/intern_list/InternListPageOne'
 
 export default function App() {
     // const [showAlert, setShowAlert] = useState(true);
@@ -21,5 +22,7 @@ export default function App() {
 
     // console.log(data[0])
 
-    return <div className="w-full"></div>;
+    return <div className="w-full">
+        <InternListPageOne/>
+    </div>;
 }
