a626ca49ad0060ad09568ff1fcbe1bc04f3aa639
Patience-Kori1 <koribiram@yahoo.fr>
2025-11-03 11:13:18 +0100

    feat(intern-list): dynamic addition of interns via form - Created global 'data' state to manage interns list - Implemented FormAddIntern component with controlled inputs - Added handleAddIntern function to append new intern to table - Linked form and table via onAddIntern and visibilityAddForm props

--- src-react/components/intern_list/FormAddIntern.jsx
+++ src-react/components/intern_list/FormAddIntern.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
-import Input from '../ui/Input'
+import { useState } from 'react'
 import Label from '../ui/Label'
 import Container from '../ui/Container'
 
-function FormAddIntern({visibilityAddForm}) {
+function FormAddIntern({visibilityAddForm, onAddIntern}) {
+    
+    const [formData, setFormData] = useState({
+        last_name: "",
+        first_name: "",
+        email:"",
+        intern_member_id: "",
+        status: "",
+    });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (onAddIntern) {
+      onAddIntern(formData);
+      setFormData({ first_name: "", intern_member_id: "", status: "Aucune demande" }); // reset
+    }
+  };
+
   return (
-    <div className= {visibilityAddForm ||"w-full"}>
+    <div className= {visibilityAddForm ||"w-full w-full transition-all duration-1000 ease-in-out max-h-[700px] opacity-100 translate-y-0"}>
         {console.log(visibilityAddForm)}
         <Container>
-            <div className=" flex flex-col w-full items-start gap-4">
+            <form onSubmit={handleSubmit}  className=" flex flex-col w-full items-start gap-4">
 
                 {/* Titre formulaire Ajout */}
                 <Label
@@ -18,7 +40,7 @@ function FormAddIntern({visibilityAddForm}) {
                     color={"primary-text"}
                 />
 
-                {/* Input nom */}
+                {/* Input et label pour nom */}
                 <div className="space-y-3 w-full ">
                     <Label
                         text= {"Nom"}
@@ -27,9 +49,15 @@ function FormAddIntern({visibilityAddForm}) {
                         color={"primary-text"}
                         labelFor={"last_name"}
                     />
-                    <input type="text" className="py-2.5 sm:py-3 px-4 block w-full border-1 border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 placeholder:text-lg placeholder:font-semibold placeholder:text-secondary-text" placeholder="Nom*" />
+                    <input type="text" 
+                        className="py-2.5 sm:py-3 px-4 block w-full border-1 border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 placeholder:text-lg placeholder:font-semibold placeholder:text-secondary-text" placeholder="Nom*" 
+                        name="last_name"
+                        value = {formData.last_name}
+                        onChange= {handleChange}
+                    />
+                    
                 </div>
-            
+
                 {/* Input prenom */}
                  <div className="w-full space-y-3">
                     <Label
@@ -39,7 +67,13 @@ function FormAddIntern({visibilityAddForm}) {
                         color={"primary-text"}
                         labelFor={"first_name"}
                     />
-                    <input type="text" className="py-2.5 sm:py-3 px-4 block w-full border-1 border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 placeholder:text-lg placeholder:font-semibold placeholder:text-secondary-text" placeholder="Prenom*" />
+                    <input 
+                        type="text" 
+                        className="py-2.5 sm:py-3 px-4 block w-full border-1 border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 placeholder:text-lg placeholder:font-semibold placeholder:text-secondary-text" placeholder="Prenom*" 
+                        name="first_name"
+                        value = {formData.first_name}
+                        onChange= {handleChange}
+                    />
                 </div>
             
                 {/* Input email */}
@@ -51,7 +85,13 @@ function FormAddIntern({visibilityAddForm}) {
                         color={"primary-text"}
                         labelFor={"email"}
                     />
-                    <input type="text" className="py-2.5 sm:py-3 px-4 block w-full border-1 border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 placeholder:text-lg placeholder:font-semibold placeholder:text-secondary-text" placeholder="Mail*" />
+                    <input 
+                        type="text" 
+                        className="py-2.5 sm:py-3 px-4 block w-full border-1 border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 placeholder:text-lg placeholder:font-semibold placeholder:text-secondary-text" placeholder="Mail*"
+                        name="email"
+                        value = {formData.email}
+                        onChange= {handleChange}
+                    />
                 </div>
                 {/* Input N° de stagiaire */}
 
@@ -63,16 +103,48 @@ function FormAddIntern({visibilityAddForm}) {
                         color={"primary-text"}
                         labelFor={"intern_member_id"}
                     />
-                    <input type="text" className="py-2.5 sm:py-3 px-4 block w-full border-1 border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 placeholder:text-lg placeholder:font-semibold placeholder:text-secondary-text" placeholder="ID*" />
+
+                    <input 
+                        type="text" 
+                        className="py-2.5 sm:py-3 px-4 block w-full border-1 border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 placeholder:text-lg placeholder:font-semibold placeholder:text-secondary-text" placeholder="ID*" 
+                        name="intern_member_id"
+                        value = {formData.intern_member_id}
+                        onChange= {handleChange}
+                    />
+                </div>
+                <div className="space-y-3 w-full ">
+                    <Label
+                        text= {"Status de la demande"}
+                        weight={"semibold"}
+                        size={"sm"}
+                        color={"primary-text"}
+                        labelFor={"status"}
+                    />
+                    <select 
+                        id="hs-select-label" 
+                        className="py-2.5 sm:py-3 px-4 block w-full border-gray-200 outline-gray-200 rounded-lg sm:text-sm focus:border-secondary focus:ring-secondary disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                        name="status"
+                        value = {formData.status}
+                        onChange= {handleChange}
+                    >
+                        {/* <option selected="">Avancée du dossier </option> */}
+                        <option defaultValue >Aucune demande</option>
+                        <option>Stagiaire a initié la demande</option>
+                        <option>Transmis à l'administration</option>
+                        <option>En attente de validation</option>
+                        <option>Terminé</option>
+                    </select>
                 </div>
 
                 {/* Bouton de soumission */}
-                <button type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-lg font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                <button 
+                    type="submit" 
+                    className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-lg font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                >
                     Envoyer l'invitation
                 </button>
         
-            </div>
-            
+            </form>
         </Container>     
     </div>
   )
--- src-react/components/intern_list/InternListContentOne.jsx
+++ src-react/components/intern_list/InternListContentOne.jsx
@@ -8,43 +8,56 @@ import { Edit } from "lucide-react";
 
 function InternListContentOne() {
 
+  // Constante concernant les titres des colonnes du tableau
   const columns = [ 
-    { key: "first_name", label: "STAGIAIRE" },
+    { key: "first_name", label: "PRENOM STAGIAIRE" },
+    { key: "last_name", label: "NOM STAGIAIRE" },
+    { key: "email", label: "EMAIL" },
     { key: "intern_member_id", label: "IDENTIFIANT" },
     { key: "status", label: "AVANCÉE DU DOSSIER" },
     { key: "action", label: "MODIFIER" },
   ]
-  const data = [
 
-    { first_name: "Jeremie Chabanais", intern_member_id: 125242, status: "Aucune demande" },
-    { first_name: "Saria Chabanais", intern_member_id: 125242, status: "Aucune demande" },
-    { first_name: "Aziza Chabanais", intern_member_id: 125242, status: "Aucune demande" },
-    { first_name: "Margot Chabanais", intern_member_id: 125242, status: "Aucune demande" },
-    { first_name: "Mounir Chabanais", intern_member_id: 125242, status: "Aucune demande" },
-    { first_name: "Amine Chabanais", intern_member_id: 125242, status: "Aucune demande" },
-    { first_name: "Karim Chabanais", intern_member_id: 125242, status: "Aucune demande" },
-    { first_name: "Koribirama Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  // Constante concernant les données des stagiaires dans le tableau
+  const [data, setData] = useState([
+    { first_name: "Saria", last_name: "Chabanais", email: "abc@gmail.com", intern_member_id: 125242, status: "Aucune demande" },
+   
 
-    // { id: 2, first_name: "Saria", last_name: "Chabanais", intern_member_id: 125242, status: "Stagiaire a initié la demande" },
-    // { id: 3, first_name: "Aziza", last_name: "Chabanais", intern_member_id: 125242, status: "Transmis à l'administration" },
-    // { id: 4, first_name: "Margot", last_name: "Chabanais", intern_member_id: 125242, status: "Aucune demande" },
-    // { id: 5, first_name: "Arnaud", last_name: "Chabanais", intern_member_id: 125242, status: "Terminé" },
-    // { id: 6, first_name: "Yves", last_name: "Dupont", intern_member_id: 125243, status: "En attente de validation" },
-    // { id: 7, first_name: "Camille", last_name: "Durand", intern_member_id: 125244, status: "Terminé" },
-    // { id: 8, first_name: "Paul", last_name: "Martin", intern_member_id: 125245, status: "Transmis à l'administration" },
-  ]
+  ])
+  console.log(data)
+  // const data = [
+
+  //   { first_name: "Jeremie Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  //   { first_name: "Saria Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  //   { first_name: "Aziza Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  //   { first_name: "Margot Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  //   { first_name: "Mounir Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  //   { first_name: "Amine Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  //   { first_name: "Karim Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  //   { first_name: "Koribirama Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+
+  //   // { id: 2, first_name: "Saria", last_name: "Chabanais", intern_member_id: 125242, status: "Stagiaire a initié la demande" },
+  //   // { id: 3, first_name: "Aziza", last_name: "Chabanais", intern_member_id: 125242, status: "Transmis à l'administration" },
+  //   // { id: 4, first_name: "Margot", last_name: "Chabanais", intern_member_id: 125242, status: "Aucune demande" },
+  //   // { id: 5, first_name: "Arnaud", last_name: "Chabanais", intern_member_id: 125242, status: "Terminé" },
+  //   // { id: 6, first_name: "Yves", last_name: "Dupont", intern_member_id: 125243, status: "En attente de validation" },
+  //   // { id: 7, first_name: "Camille", last_name: "Durand", intern_member_id: 125244, status: "Terminé" },
+  //   // { id: 8, first_name: "Paul", last_name: "Martin", intern_member_id: 125245, status: "Transmis à l'administration" },
+  // ]
+
+  // Constante concernant la gestion du composant calendar sur les dates de formation
   const [currentFormation, ] = useState({
       periodStart: new Date(2026, 0, 5),  // 5 janvier 2026
       periodEnd: new Date(2026, 2, 27),   // 27 mars 2026
   });
 
-  // état pour contrôler la visibilité
+  // état pour contrôler la visibilité du formulaire au niveau du bouton du composant AppTable (au click il le change il le met en true) et du composant FormAddIntern au click du bouton de AppTable, FormAdd le perçois dans showForm et active la visibilité du formulaire
   const [showForm, setShowForm] = useState(false)
-  console.log(showForm)
+  // console.log(showForm)
   
   // Fonction déclenchée depuis le bouton du tableau
   const handleAddInternClick = () => {
-    setShowForm(!showForm) // alterne visible / caché
+    setShowForm(!showForm) // alterne visible (true) / caché (false)
   }
  
   const addIntern = (keyAction) => {
@@ -52,6 +65,7 @@ function InternListContentOne() {
   }
   // console.log(keyAction)
   
+  // La constante pour le bouton du composant tableau qui est dans la colonne action
   const divAction = 
   (
     <div className="flex gap-3 justify-start">
@@ -63,13 +77,23 @@ function InternListContentOne() {
     </button>
     </div>
   ) 
+
+  // Fonction appelée quand le formulaire est soumis
+  const handleAddIntern = (newIntern) => {
+    // On ajoute le nouveau stagiaire dans le tableau existant
+    setData(prev => [...prev, newIntern])
+    setShowForm(false)
+  }
   
   return (
     
     <div className="ml-4">
+      {/* Composant contenant le titre de la page et la barre de progression */}
       <HeaderInternList/>
 
       <div className="flex flex-row items-start gap-4">
+
+        {/* Composant contenant le tableau et personnalisé pour cette page */}
         <AppTable
           classNameTableTitle={"hidden"}  
           columns = {columns}
@@ -81,14 +105,15 @@ function InternListContentOne() {
         />
 
         <div className="flex flex-col items-start gap-4 mr-8">  
-{/*           
+          
           <FormAddIntern
             // visibilityAddForm= {visibilityAddForm}
             visibilityAddForm={showForm ? "visible w-full" : "hidden"}
-          /> */}
+            onAddIntern={handleAddIntern}
+          />
 
           {/* --- Transition douce du formulaire --- */}
-          <div
+          {/* <div
             className={`w-full transition-all duration-1000 ease-in-out ${
               showForm
                 ? "max-h-[700px] opacity-100 translate-y-0"
@@ -96,7 +121,7 @@ function InternListContentOne() {
             }`}
           >
             <FormAddIntern />
-          </div>
+          </div> */}
 
           <CalendarSimpleGET
             dates={currentFormation}
