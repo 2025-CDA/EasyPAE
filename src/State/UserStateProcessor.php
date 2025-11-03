45b1a6c2b3838c677e68f8b28d5041f674286066
M-BEDH <mel.b.ms@pm.me>
2025-11-03 16:05:15 +0100

    fix method GET (method undefined) avec requestStack

--- src/State/UserStateProcessor.php
+++ src/State/UserStateProcessor.php
@@ -8,22 +8,30 @@ use App\Entity\User;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-
+// Added to access the current HTTP request
+use Symfony\Component\HttpFoundation\RequestStack;
 
 readonly class UserStateProcessor implements ProcessorInterface
 {
     public function __construct(
         private ProcessorInterface $persistProcessor,
         private readonly UserPasswordHasherInterface $passwordHasher,
-        private MailerInterface $mailer
-    ) {
-    }
+        private MailerInterface $mailer,
+        // Inject RequestStack to access the current request
+        private RequestStack $requestStack
+    ) {}
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): mixed
     {
-        if ($data instanceof User &&
+        // Get the current HTTP request method
+        $request = $this->requestStack->getCurrentRequest();
+        $httpMethod = $request?->getMethod();
+
+        if (
+            $data instanceof User &&
             $data->getPlainPassword() &&
-            ($operation->getMethod() === 'POST' || $operation->getMethod() === 'PATCH')
+            // Only hash password on creation or update
+            ($httpMethod === 'POST' || $httpMethod === 'PATCH')
         ) {
             $hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPlainPassword());
             $data->setPassword($hashedPassword);
@@ -32,7 +40,7 @@ readonly class UserStateProcessor implements ProcessorInterface
 
         $persistedUser = $this->persistProcessor->process($data, $operation, $uriVariables, $context);
 
-        if ($persistedUser instanceof User && $operation->getMethod() === 'POST') {
+        if ($persistedUser instanceof User && $httpMethod === 'POST') {
             $message = (new Email())
                 ->from('registration@easypae.com')
                 ->to($persistedUser->getEmail())
