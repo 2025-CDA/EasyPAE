3f7d6e77927243533405459b5702ca63532c6678
maxc0434 <max.couillet@gmail.com>
2025-11-04 16:05:25 +0100

    notification company for infoForm validated

--- src/Service/NotificationService.php
+++ src/Service/NotificationService.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Entity\InfoForm;
 use App\Entity\Notification;
 use App\Entity\InfoFormIntern;
+use App\Entity\TrainingSession;
 use App\Entity\UserNotification;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -80,4 +81,68 @@ class NotificationService
         $this->em->persist($userNotification);
         $this->em->flush();
     }
+
+    public function sendInfoFormValidatedCompanyNotification(User $user, InfoForm $infoForm, InfoFormIntern $infoFormIntern): void
+    {
+
+        // Récupère les dates depuis l'objet InfoFormIntern et formate proprement
+        $dateStringStart = '';
+        $dateStringEnd = '';
+
+        $ds = $infoFormIntern->getDateStart();
+        if ($ds instanceof \DateTimeInterface) {
+            $dateStringStart = $ds->format('d-m-Y');
+        } elseif (is_string($ds) && $ds !== '') {
+            try {
+                $dt = new DateTime($ds);
+                $dateStringStart = $dt->format('d-m-Y');
+            } catch (\Exception $e) {
+                $dateStringStart = $ds;
+            }
+        }
+
+        $de = $infoFormIntern->getDateEnd();
+        if ($de instanceof \DateTimeInterface) {
+            $dateStringEnd = $de->format('d-m-Y');
+        } elseif (is_string($de) && $de !== '') {
+            try {
+                $dt = new DateTime($de);
+                $dateStringStart = $dt->format('d-m-Y');
+            } catch (\Exception $e) {
+                $dateStringStart = $de;
+            }
+        }
+
+        $internFirstName = $infoForm->getInternMember()->getUser()->getFirstName();
+        $internLastName = $infoForm->getInternMember()->getUser()->getLastName();
+
+        $title = 'Fiche de renseignement validée';
+        $content = "Le stage de {$internFirstName} {$internLastName} du {$dateStringStart} au {$dateStringEnd} au sein de votre entreprise a été validé.";
+
+        $userNotification = new UserNotification();
+        $userNotification->setUser($user);
+
+        // setters usuels : adapter si noms différents dans votre entité
+        if (method_exists($userNotification, 'setTitle')) {
+            $userNotification->setTitle($title);
+        }
+        if (method_exists($userNotification, 'setContent')) {
+            $userNotification->setContent($content);
+        }
+
+        if (method_exists($userNotification, 'setCreatedAt')) {
+            $userNotification->setCreatedAt(new \DateTimeImmutable());
+        }
+        if (method_exists($userNotification, 'setIsRead')) {
+            $userNotification->setIsRead(false);
+        }
+        if (method_exists($userNotification, 'setIsSigned')) {
+            $userNotification->setIsSigned(false);
+        }
+
+        $this->em->persist($userNotification);
+        $this->em->flush();
+    }
+
+
 }
