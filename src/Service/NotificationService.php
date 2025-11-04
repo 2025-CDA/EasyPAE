51e1acf40f245c608f839bc83c243539cf3308b6
maxc0434 <max.couillet@gmail.com>
2025-11-04 14:02:22 +0100

    notifications

--- src/Service/NotificationService.php
+++ src/Service/NotificationService.php
@@ -2,9 +2,12 @@
 
 namespace App\Service;
 
-use App\Entity\Notification;
+use DateTime;
 use App\Entity\User;
 use App\Entity\InfoForm;
+use App\Entity\Notification;
+use App\Entity\InfoFormIntern;
+use App\Entity\UserNotification;
 use Doctrine\ORM\EntityManagerInterface;
 
 class NotificationService
@@ -14,11 +17,67 @@ class NotificationService
     ) {}
 
     /**
-     * Crée une notification pour l'utilisateur.
+     * Crée une notification pour l'utilisateur lorsqu'un InfoForm est validé.
+     * Stocke title et content dans la table user_notification (entité UserNotification).
      */
-    public function sendInfoFormValidatedNotification(User $user, InfoForm $infoForm): void
+    public function sendInfoFormValidatedInternNotification(User $user, InfoForm $infoForm, InfoFormIntern $infoFormIntern): void
     {
 
+        // Récupère les dates depuis l'objet InfoFormIntern et formate proprement
+        $dateStringStart = '';
+        $dateStringEnd = '';
 
+        $ds = $infoFormIntern->getDateStart();
+        if ($ds instanceof \DateTimeInterface) {
+            $dateStringStart = $ds->format('d-m-Y');
+        } elseif (is_string($ds) && $ds !== '') {
+            try {
+                $dt = new DateTime($ds);
+                $dateStringStart = $dt->format('d-m-Y');
+            } catch (\Exception $e) {
+                $dateStringStart = $ds;
+            }
+        }
+
+        $de = $infoFormIntern->getDateEnd();
+        if ($de instanceof \DateTimeInterface) {
+            $dateStringEnd = $de->format('d-m-Y');
+        } elseif (is_string($de) && $de !== '') {
+            try {
+                $dt = new DateTime($de);
+                $dateStringStart = $dt->format('d-m-Y');
+            } catch (\Exception $e) {
+                $dateStringStart = $de;
+            }
+        }
+
+        $companyName = $infoForm->getCompanyMembers()->first()->getCompany()->getName();
+
+        $title = 'Fiche de renseignement validée';
+        $content = "Le stage du {$dateStringStart} au {$dateStringEnd} , au sein de l'entreprise {$companyName} a été validé.";
+
+        $userNotification = new UserNotification();
+        $userNotification->setUser($user);
+
+        // setters usuels : adapter si noms différents dans votre entité
+        if (method_exists($userNotification, 'setTitle')) {
+            $userNotification->setTitle($title);
+        }
+        if (method_exists($userNotification, 'setContent')) {
+            $userNotification->setContent($content);
+        }
+
+        if (method_exists($userNotification, 'setCreatedAt')) {
+            $userNotification->setCreatedAt(new \DateTimeImmutable());
+        }
+        if (method_exists($userNotification, 'setIsRead')) {
+            $userNotification->setIsRead(false);
+        }
+        if (method_exists($userNotification, 'setIsSigned')) {
+            $userNotification->setIsSigned(false);
+        }
+
+        $this->em->persist($userNotification);
+        $this->em->flush();
     }
 }
